22ebea3cba8a5ab329cf407aea49855800845a80
callum-i <callum.iddon@stfc.ac.uk>
2017-06-02 20:49:15 +0100

    Update to pycryptodome

--- test_utils.py
+++ test_utils.py
@@ -3,42 +3,35 @@ import pytest
 import os
 
 
-def test_getKey():
-    # Check that it doesnt accept anything but SALT_LENGTH
-    for i in (list(range(utils.SALT_LENGTH)) +
-              list(range(utils.SALT_LENGTH + 1, 50))):
-        with pytest.raises(AssertionError):
-            utils.getKey("password", b"\x00" * i)
-    # Check that it accepts SALT_LENGTH
-    utils.getKey("password", b"\x00" * utils.SALT_LENGTH)
-    # Check that it accepts different salt
-    utils.getKey("password", (b"\x01\x02\x03\x04" + b"\x01" *
-                              (utils.SALT_LENGTH - 4)))
-
-
 def test_setupCipher():
     # Check that this standard call doesn't raise errors
-    key = utils.getKey("password", b"\x00" * utils.SALT_LENGTH)
-    iv = "\x00" * utils.IV_LENGTH
-    utils.setupCipher(key, iv)
-
-
-def test_setupMAC():
-    # Check this standard call doesn't raise errors
-    key = utils.getKey("password", b"\x00" * utils.SALT_LENGTH)
-    content = b"Hi"
-    utils.setupMAC(key, content)
+    iv = b"\x00" * utils.IV_LENGTH
+    utils.setupCipher("password", b"\x00" * utils.SALT_LENGTH, iv)
 
 
 def test_encryptToFile_writesFile():
-    name = "test.txt"
+    # Tests encryptToFile() actually makes a file.
+    name = "test.enc"
     utils.encryptToFile(name, "this is a test", "password")
     assert os.path.isfile(name)
 
 
 def test_encrypt_decrypt():
-    fname = "test.txt"
+    # Tests encrypt(decrypt(plaintext)) == plaintext
+    fname = "test.enc"
     content = "this is a test"
     password = "password"
     utils.encryptToFile(fname, content, password)
-    assert utils.decryptFromFile(fname, password) == content
+    assert utils.decryptFromFile(fname, password).decode() == content
+
+
+def test_mac():
+    # Tests the cipher text can't be modified without alerting the MA
+    fname = "test.enc"
+    content = "this is a test"
+    password = "password"
+    utils.encryptToFile(fname, content, password)
+    with open(fname, "ab") as fhandle:
+        fhandle.write(b"additional stuff")
+    with pytest.raises(ValueError):
+        utils.decryptFromFile(fname, password)
